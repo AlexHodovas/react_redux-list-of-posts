577ecda96cb58f2c5ad3267d79f24c1f43ed8a21
AlexHodovas <o.hodovas@gmail.com>
2020-01-11 20:44:07 +0200

    Updates

---
+++ precache-manifest.a24810944e6a1170d021d02c3969961b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56b77dcec110f2ff55ef3af7bbf9f48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a1abf44b5f5ac7a7f4d",
+    "url": "./static/css/main.44b3de83.chunk.css"
+  },
+  {
+    "revision": "404630e2f19e402fbff8",
+    "url": "./static/js/2.e8cc22a1.chunk.js"
+  },
+  {
+    "revision": "3a1abf44b5f5ac7a7f4d",
+    "url": "./static/js/main.ce15a9ab.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
